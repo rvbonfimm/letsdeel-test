fd3b0aa2031cac8f2219792df60198057b53bbe7
rvbonfimm <rogeriovilelabonfim@gmail.com>
2021-10-09 12:43:42 -0300

    feat: add jobs/unpaid endpoint

--- src/routes.js
+++ src/routes.js
@@ -57,4 +57,49 @@ router.get('/contracts', getProfile, async (req, res) => {
     res.json(contracts)
 })
 
+/**
+ * Get all unpaid jobs for a user (either a client or contractor).
+ * For active contracts only.
+ * @returns jobs
+ */
+router.get('/jobs/unpaid', getProfile, async (req, res) => {
+    const { Contract, Job } = req.app.get('models')
+
+    const jobs = await Job.findAll({ where: {
+        paid: null
+    } })
+
+    if (!jobs) return res.status(404).end()
+
+    let filters = {
+        status: { [Op.in]: ['new', 'in_progress'] }
+    }
+    const profile = req.profile
+    const profileType = profile.type
+    switch (profileType) {
+        case "client":
+            filters["ClientId"] = profile.id
+            break
+        case "contractor":
+            filters["ContractorId"] = profile.id
+            break
+    }
+
+    /** Search for Active Contracts only */
+    let desiredJobs = []
+    for await (const job of jobs) {
+        const contractId = job.ContractId
+
+        filters['id'] = contractId
+
+        const contract = await Contract.findOne({ where: filters })
+        if (!contract) continue
+
+        desiredJobs.push(job)
+    }
+
+    if (desiredJobs.length === 0) return res.status(404).end()
+    res.json(desiredJobs)
+})
+
 module.exports = router
