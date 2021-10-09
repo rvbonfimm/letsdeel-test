152de02616df6313a05de6cf4459e29888e13362
rvbonfimm <rogeriovilelabonfim@gmail.com>
2021-10-09 11:49:49 -0300

    fix: query contracts by user profile id and type

--- src/app.js
+++ src/app.js
@@ -8,13 +8,26 @@ app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
+ * It should return the contract only if it belongs to the profile calling
  * @returns contract by id
  */
 app.get('/contracts/:id',getProfile ,async (req, res) =>{
     const {Contract} = req.app.get('models')
     const {id} = req.params
-    const contract = await Contract.findOne({where: {id}})
+
+    let filters = { id: parseInt(id) }
+    const profile = req.profile
+    const profileType = profile.type
+    switch (profileType) {
+        case "client":
+            filters["ClientId"] = profile.id
+            break
+        case "contractor":
+            filters["ContractorId"] = profile.id
+            break
+    }
+
+    const contract = await Contract.findOne({where: filters})
     if(!contract) return res.status(404).end()
     res.json(contract)
 })
