9f9aeeb68d0cd1ad3e11875ed5210f163d06b9d5
rvbonfimm <rogeriovilelabonfim@gmail.com>
2021-10-09 13:57:04 -0300

    feat: add jobs/pay endpoint

--- src/routes.js
+++ src/routes.js
@@ -102,4 +102,64 @@ router.get('/jobs/unpaid', getProfile, async (req, res) => {
     res.json(desiredJobs)
 })
 
+// TODO improve find+update operation to use only one
+/**
+ * Pay for a job (Client to contractor)
+ * A client can only pay if his balance >= the amount to pay
+ * The amount should be moved from the client's balance to the contractor balance
+ * @returns operation status
+ */
+router.post('/jobs/:job_id/pay', getProfile, async (req, res) => {
+    const { Job, Contract, Profile } = req.app.get('models')
+    const sequelize = req.app.get('sequelize')
+    const { id } = req.params
+
+    const job = await Job.findOne({ where: id })
+    if (!job) return res.status(404).end()
+
+    /** Check for job already opened */
+    if (job.paid !== null) return res.json("Job already paid")
+
+    /** Check for client balance */
+    const profile = req.profile
+    if (profile.balance < job.price) {
+        return res.json("You do not have sufficient money to pay the job")
+    }
+
+    /** Send profile money to the job contractor */
+    const contract = await Contract.findOne({ where: { id: job.ContractId } })
+    if (!contract) {
+        return res.status(404).json({ 
+            error: "Contract for the payment not found" 
+        }).end()
+    }
+
+    const clientId = contract.ClientId
+    const contractorId = contract.ContractorId
+
+    const transaction = await sequelize.transaction();
+    const jobClient = await Profile.findOne({ where: { id: clientId } })
+    const jobContractor = await Profile.findOne({ where: { id: contractorId } })
+    try {
+        /** Withdraw from client */
+        const clientBalance = jobClient.balance - job.price
+        await Profile.update(
+            { balance: clientBalance }, 
+            { where: { id: clientId } }, { transaction }
+        )
+
+        /** Deposit to contractor */
+        const contractorBalance = jobContractor.balance + job.price
+        await Profile.update(
+            { balance: contractorBalance },
+            { where: { id: contractorId } }
+        )
+
+        return res.json({ status: "success" })
+    } catch (error) {
+        await transaction.rollback()
+        return res.json({ error })
+    }
+})
+
 module.exports = router
